614407458227fe13a4dcf00a754b4f4f00557757
ivansvetlichnyi <redjohnfrv@gmail.com>
2022-01-06 14:16:22 +0600

    refactor: change structure  - add Stuff.tsx  - rename MainScreen to Menu and add it into Navigator.tsx

--- src/main/Navigator.tsx
+++ src/main/Navigator.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import {Route, Routes } from 'react-router-dom'
-import {MainScreen} from './MainScreen'
+import {Menu} from './Menu'
 import {Jewelry} from './Jewelry'
-import {TO_JEW, TO_ROOT} from '../constants'
+import {Stuff} from './Stuff'
+import {TO_JEW, TO_ROOT, TO_STUFF} from '../constants'
+import {Container} from '../ui'
 
 export const Navigator = () => {
   return (
-    <Routes>
-      <Route path={TO_ROOT} element={<MainScreen />} />
-      <Route path={TO_JEW} element={<Jewelry />} />
-    </Routes>
+    <Container>
+      <Menu />
+      <Routes>
+        <Route path={TO_ROOT} element={<Jewelry />} />
+        <Route path={TO_JEW} element={<Jewelry />} />
+        <Route path={TO_STUFF} element={<Stuff />} />
+      </Routes>
+    </Container>
   )
 }
